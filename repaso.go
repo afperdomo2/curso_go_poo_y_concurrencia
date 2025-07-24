7bd3933296e98bd85467aea2a89fb1f5018b95c2
alvarado0925 <alvarado0925@gmail.com>
2025-07-24 11:55:01 -0500

    Mejorar la presentación de la salida y agregar comentarios para el uso de goroutines y punteros

--- repaso.go
+++ repaso.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -29,9 +30,10 @@ func main() {
 	phoneDirectory := make(map[string]int)
 	phoneDirectory["Alice"] = 123456789
 	phoneDirectory["Bob"] = 987654321
-	fmt.Println("Phone Directory:", phoneDirectory)
+	fmt.Println("\nPhone Directory:", phoneDirectory)
 
 	// 4. Uso de Slices (números primos)
+	fmt.Println("\nNúmeros primos:")
 	primes := []int{2, 3, 5, 7, 11, 13}
 
 	primes = append(primes, 17, 19) // Agregar más números primos
@@ -39,4 +41,26 @@ func main() {
 	for i, prime := range primes {
 		fmt.Printf("Prime number at index %d: %d\n", i, prime)
 	}
+
+	// 5. Uso Goroutines y canales
+	fmt.Println("\nIniciando goroutine...")
+	c := make(chan int) // Canal para comunicación entre goroutines
+	go doSomething(c)   // Esta goroutine permite crearla pero no esperar su finalización
+	<-c                 // Espera a que la goroutine envíe un valor al canal
+	fmt.Println("Fin")
+
+	// 6. Uso de Punteros
+	var a int = 10
+	var b *int = &a
+	fmt.Println("\nValor de a:", a)
+	fmt.Println("Dirección de a:", &a)
+	fmt.Println("Valor de b:", *b)
+	fmt.Println("Dirección de b:", b)
+	fmt.Println(("A es igual a B?"), a == *b)
+}
+
+func doSomething(c chan int) {
+	time.Sleep(1 * time.Second) // Simula una tarea que toma tiempo
+	fmt.Println("Tarea completada")
+	c <- 42 // Envía un valor al canal
 }
